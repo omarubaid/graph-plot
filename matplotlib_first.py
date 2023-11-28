c7d29ae697a71cbca5a11d12a7b24455baedd54d
Omar Ubaid <omarubaid1997@gmail.com>
2023-11-28 14:34:09 +0100

    feat: graph plot using matplotlib

---
+++ matplotlib_first.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 4, 6, 8, 10]
+
+# Plotting the data
+plt.plot(x_values, y_values)
+
+# Adding labels and title
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('Simple Line Graph')
+
+# Display the plot
+plt.show()
